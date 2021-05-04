992d47ae2ac87174182cb377590329233e6b3da7
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-04 14:02:55 +0200

    correction exo variables temporaires

---
+++ src/js/main.js
@@ -0,0 +1,66 @@
+// exo 1
+//let a = 1;
+//let b = 2;
+//let c = 3;
+//let d = 4;
+
+// temp = a;
+//a = d;
+//d = temp;
+// temp = b;
+//b = c;
+//c = temp;
+
+//console.log(a, b, c, d);
+
+// exo 2
+//let a = 1;
+//let b = 2;
+//let c = 3;
+//let d = 4;
+
+//let temp = a;
+
+//a = b;
+//b = c;
+//c = d;
+//d = temp;
+
+//console.log(a, b, c, d);
+
+//exo 3
+
+let tab = [1, 2, 3, 4, "temp"];
+tab[4] = tab[0];
+tab[0] = tab[3];
+tab[3] = tab [4];
+tab[4] = tab[1];
+tab[1] = tab[2];
+tab[2] = tab[4];
+console.log(tab[0], tab[1], tab[2], tab[3])
+
+
+// exo 4
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 6;
+
+// let temp = a;
+
+// a = d;
+// d = b;
+// b = e;
+// e = f;
+// f = c;
+// c = temp;
+
+
+
+
+// console.log(a, b, c, d, e, f);
+
+
